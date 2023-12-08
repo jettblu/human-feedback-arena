3b7c6ae9e461b0c5df108ae58ce41bc4853ce563
jettblu <jettblu.fabrications@gmail.com>
2023-12-08 15:48:53 -0500

    Add icon

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -6,8 +6,10 @@ export const Legible_Font = Inter({ style: "normal", subsets: ["latin"] });
 export const Game_Font = Press_Start_2P({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "RLHF in the Browser",
-  description: "A browser-based version of reinforcement with human feedback.",
+  title: "Preference Arcade",
+  description:
+    "Incorporate human preferences into autonomous agents with online game play.",
+  icons: ["/favicon.png"],
 };
 
 export default function RootLayout({
