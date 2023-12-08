f8054ebf331e140d40a9450a5f81547cdddcc09a
jettblu <jettblu.fabrications@gmail.com>
2023-12-07 22:47:11 -0500

    Add sounds and observation progress bar

--- frontend/app/experiments/new/page.tsx
+++ frontend/app/experiments/new/page.tsx
@@ -6,13 +6,13 @@ import { useDispatch, Provider, useSelector } from "react-redux";
 import store from "@/store";
 import ScoreCard from "@/components/games/snake/Scorecard";
 import { useEffect, useState } from "react";
-import Instruction from "@/components/games/snake/Instructions";
 import { IGlobalState } from "@/store/reducers";
 import { getObservation } from "@/helpers/data";
 import { BOARD_HEIGHT, BOARD_WIDTH } from "@/helpers/constants";
 import { reverseDirection } from "@/utils";
 import { Direction } from "@/types";
 import { addObservation } from "@/store/actions";
+import CreateExperimentCard from "@/components/games/snake/CreateExperimentCard";
 
 // import snake game from the phaser lib
 enum Task {
@@ -31,20 +31,24 @@ export default function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col p-8 mt-8">
+    <main className="flex min-h-screen flex-col px-2 mt-8">
       <div className="max-w-5xl mx-auto w-full">
         <Provider store={store}>
           {task_human === Task.introduction && (
             <div className="mx-auto w-fit">
-              <Instruction onBegin={habndleDoneWithIntro} />
-              {/* link back home here */}
-              <div className="w-fit mt-8 mx-auto">
-                <Link
-                  href="/"
-                  className="text-md hover:underline hover:text-blue-600 transition duration-100 text-center"
-                >
-                  Back to Home
-                </Link>
+              {/* <Instruction onBegin={habndleDoneWithIntro} /> */}
+              {/* form with option to input experiment name and description in retro style */}
+              <div className="flex flex-col items-center justify-center min-h-screen max-w-5xl mx-auto lg:-mt-32">
+                <CreateExperimentCard onBegin={habndleDoneWithIntro} />
+                {/* link back home here */}
+                <div className="w-fit mt-8 mx-auto">
+                  <Link
+                    href="/"
+                    className="text-md hover:underline hover:text-blue-600 transition duration-100 text-center"
+                  >
+                    Back to Home
+                  </Link>
+                </div>
               </div>
             </div>
           )}
@@ -77,12 +81,21 @@ function TrainingBoard(params: { handleGameEnd: () => void }) {
     (state: IGlobalState) => state.obersvationsRequired
   );
   const observations = useSelector((state: IGlobalState) => state.observations);
+
+  // load audio clips for game
+  const consumedFood = new Audio("/snake/collect.mp3");
+  const gameOver = new Audio("/snake/game_over.mp3");
+  const gamePlay = new Audio("/snake/gameplay.mp3");
+  const backgroundMusic = new Audio("/snake/background-music.mp3");
+  gamePlay.loop = true;
+  backgroundMusic.loop = true;
+
   // capture reward changing observations
   useEffect(() => {
-    if (lastReward != 1 || lastReward != 1 || !snake || !lastSnake) {
+    if ((lastReward != 1 && lastReward != -1) || !snake || !lastSnake) {
       return;
     }
-    console.log("Gathering reward observation");
+
     const dir: Direction = reverseDirection(lastAction);
     const newObservation = getObservation({
       food,
@@ -104,10 +117,9 @@ function TrainingBoard(params: { handleGameEnd: () => void }) {
   }, [totalObservations]);
 
   useEffect(() => {
-    // every 50 moves, capture the observation
+    // every 25 moves, capture the observation
     if (counter % 25 == 0 && counter != 0 && snake && lastSnake) {
       console.log("Gathering scheduled observation");
-      console.log("counter", counter);
       const dir: Direction = reverseDirection(lastAction);
       const newObservation = getObservation({
         food,
@@ -153,8 +165,20 @@ function TrainingBoard(params: { handleGameEnd: () => void }) {
     console.log("totalObservations", totalObservations);
   }, [totalObservations, observations]);
 
+  useEffect(() => {
+    if (score > 0) {
+      consumedFood.play();
+    }
+  }, [score]);
+
+  useEffect(() => {
+    if (lastReward == -1) {
+      gameOver.play();
+    }
+  }, [lastReward]);
+
   return (
-    <div>
+    <div className="">
       {/* link back home here */}
       <Link
         href="/"
@@ -168,22 +192,27 @@ function TrainingBoard(params: { handleGameEnd: () => void }) {
       <h1 className="text-4xl font-bold text-left text-green-500">
         Snake Game
       </h1>
-      <p className={`text-md text-left mb-4`}>
-        Train the snake to eat the food. The snake should avoid hitting the
-        walls and itself. Try to replicate the slithering pattern of a real
-        snake, when possible.
+      <p className={`text-md text-left mb-3`}>
+        The snake is learning to play based on your playing style. You can move
+        the snake with the arrow keys. You should follow your playing style
+        while eating the food and avoiding walls.
       </p>
-      {/* progress bar that shows how many observations we have made */}
-      <div className="w-full h-4 bg-gray-300">
-        <div
-          className="h-full bg-green-500"
-          style={{
-            width: `${(totalObservations / observationsRequired) * 100}%`,
-          }}
-        ></div>
-        <CanvasBoard height={600} width={1000} />
-        <ScoreCard />
+      <div className="w-full px-2">
+        {/* progress bar that shows how many observations we have made */}
+        <div className="w-full h-8 bg-gray-300 mb-4 relative">
+          <div
+            className="h-full bg-green-500"
+            style={{
+              width: `${(totalObservations / observationsRequired) * 100}%`,
+            }}
+          />
+          <div className="text-center text-gray-600 text-xs absolute top-2 left-8">
+            {totalObservations}/{observationsRequired} observations
+          </div>
+        </div>
       </div>
+      <CanvasBoard height={600} width={1000} />
+      <ScoreCard />
     </div>
   );
 }
